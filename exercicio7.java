d42fe8c69fbf640cd8d80b7a8cb0d405793cf8dd
resistirb <bryan.cherobini@gmail.com>
2026-03-30 11:21:26 -0300

    Add files via upload

---
+++ exercicio7.java
@@ -0,0 +1,10 @@
+public class exercicio7 {
+    public static void main(String[] args){
+        int entrada = 5;
+        if (entrada %2 ==0){
+            System.out.println("É par");
+        } else{
+            System.out.println("É ímpar");
+        }
+    }
+}
