64359ed8a0a275f783c215279e295cda851d04f1
resistirb <bryan.cherobini@gmail.com>
2026-03-31 20:07:55 -0300

    Update exercicio5.java

--- exercicio5.java
+++ exercicio5.java
@@ -4,13 +4,13 @@ public class exercicio5 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String pessoamenorpeso = "";
-        Double menorpesoatual = 0.0;
+        double menorpesoatual = 0.0;
 
         for(int i=0; i<10; i++){
             System.out.println("Nome da pessoa: ");
             String nome = sc.nextLine();
             System.out.println("Peso da pessoa: ");
-            Double peso = sc.nextDouble();
+            double peso = sc.nextDouble();
             sc.nextLine();
 
             if(i == 0){
