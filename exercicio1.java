d4fa1d8715a6ea60cc1a12962b2cfa8e262cd697
resistirb <bryan.cherobini@gmail.com>
2026-03-31 22:12:32 -0300

    Create exercicio1.java

---
+++ exercicio1.java
@@ -0,0 +1,7 @@
+public class exercicio1{
+    public static void main(String[] args){
+        int valor = 100;
+        double reajuste = (double) valor+(0.1*valor);
+        System.out.println("Valor reajustado : " + reajuste);
+    }
+}
